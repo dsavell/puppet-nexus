6373081fd6bbf561bb03372847c77dc9fa3c2627
David Savell <dsavell21@gmail.com>
2018-06-05 21:35:01 +0000

    Updates

--- manifests/params.pp
+++ manifests/params.pp
@@ -8,13 +8,13 @@
 #
 class nexus::params {
 
-  $version      = '3.11.0-01'
+  $version      = '3.12.0-01'
   $custom_path  = '/opt'
   $dir_name     = "nexus-${version}"
   $download_url = "https://download.sonatype.com/nexus/3/nexus-${version}-unix.tar.gz"
   $install_path = "${custom_path}/${dir_name}"
-  $md5_hash     = '6e15a48263e52291e3041ac755417525'
-  $java_major   = '8u171'
+  $md5_hash     = '3fa82f46c53459f6545a6dd6f213d92b'
+  $java_major   = '8u172'
   $java_minor   = 'b11'
   $user         = 'nexus'
 
